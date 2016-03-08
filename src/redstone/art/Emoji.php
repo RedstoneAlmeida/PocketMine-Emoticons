d142977843420d315d0ae0e046e65266dc4be0e4
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-03-08 20:17:18 -0300

    Create Emoji.php

---
+++ src/redstone/art/Emoji.php
@@ -0,0 +1,18 @@
+<?php		
+ namespace xbeastmode\art;		
+ use pocketmine\plugin\PluginBase;		
+ use pocketmine\event\Listener;		
+ use pocketmine\event\player\PlayerChatEvent;		
+ class Emoji extends PluginBase implements Listener{		
+     public function onEnable(){		
+         $this->saveDefaultConfig();		
+         $this->getServer()->getPluginManager()->registerEvents($this, $this);		
+     }		
+     public function onChat(PlayerChatEvent $e){		
+         if($e->getPlayer()->hasPermission("emoji.use")){		
+             foreach ($this->getConfig()->getAll() as $msg => $emoji){		
+                 $e->setMessage(str_replace($msg,$emoji,$e->getMessage()));		
+             }		
+        }		
+     }		
+ }
