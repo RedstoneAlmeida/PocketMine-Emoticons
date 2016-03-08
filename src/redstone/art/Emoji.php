c02cfe2a5721856ba579d113145e46ee3fc19d26
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-03-08 20:28:13 -0300

    Update Emoji.php

--- src/redstone/art/Emoji.php
+++ src/redstone/art/Emoji.php
@@ -1,17 +1,27 @@
 <?php		
- namespace redstone\art;		
+
+ namespace redstone\art;
+ 
  use pocketmine\plugin\PluginBase;		
  use pocketmine\event\Listener;		
  use pocketmine\event\player\PlayerChatEvent;		
+ 
  class Emoji extends PluginBase implements Listener{		
+  
      public function onEnable(){		
+      
          $this->saveDefaultConfig();		
          $this->getServer()->getPluginManager()->registerEvents($this, $this);		
+         
      }		
      public function onChat(PlayerChatEvent $e){		
+      
          if($e->getPlayer()->hasPermission("emoji.use")){		
+          
              foreach ($this->getConfig()->getAll() as $msg => $emoji){		
+              
                  $e->setMessage(str_replace($msg,$emoji,$e->getMessage()));		
+                 
              }		
         }		
      }		
